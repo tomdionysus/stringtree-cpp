6ab8a4a373a5845e7dfae1a99ebd055472969f08
Tom Cully <tom@blackraven.co.nz>
2023-02-25 14:03:07 +1300

    Updated Node, Tree, more tests

--- src/stringtree.h
+++ src/stringtree.h
@@ -8,20 +8,24 @@ using namespace std;
 namespace StringTree {
 
 	template <class T> class Node {
-		Node* left;
-		Node* right;
-		Node* forward;
-		Node* backward;
-		char key;
 		
 		public:
+			char key;
+			Node<T>* left;
+			Node<T>* right;
+			Node<T>* forward;
+			Node<T>* backward;
 			T value;
 
 			Node(char key, T value);
+			Node(char key);
 			~Node();
 
-			Node* find(char key);
+			Node<T>* find(char key);
+			Node<T>* add(char k);
 			Node<T>* add(char k, T value);
+
+			void deleteAll();
 	};
 
 	template <class T> class Tree {
@@ -35,6 +39,141 @@ namespace StringTree {
 			T get(string str);
 			void clear(string str);
 	};
+
+
+	// Node
+
+	template <class T> Node<T>::Node(char k, T v) {
+		key = k;
+		value = v;
+		left = nullptr;
+		right = nullptr;
+		forward = nullptr;
+	}
+
+	template <class T> Node<T>::Node(char k) {
+		key = k;
+		left = nullptr;
+		right = nullptr;
+		forward = nullptr;
+	}
+
+	template <class T> Node<T>::~Node() {
+		left = nullptr;
+		right = nullptr;
+		forward = nullptr;
+	}
+
+	template <class T> void Node<T>::deleteAll() {
+		if(this->forward) this->forward->deleteAll();
+		if(this->left) this->left->deleteAll();
+		if(this->right) this->right->deleteAll();
+		delete this;
+	}
+
+	template <class T> Node<T>* Node<T>::find(char k) {
+		Node* current = this;
+		while(current != nullptr) {
+			if(current->key == k) return current;
+			if(k < current->key) {
+				current = current->left;
+			} else {
+				current = current->right;
+			}
+		}
+		return nullptr;
+	}
+
+	template <class T> Node<T>* Node<T>::add(char k) {
+		Node<T>* current = this;
+		while(true) {
+			if(current->key == k) return current;
+			if(k < current->key) {
+				if(current->left) { 
+					current = current->left;
+				} else {
+					current->left = new Node(k);
+					return current->left;
+				}
+			} else {
+				if(current->right) { 
+					current = current->right;
+				} else {
+					current->right = new Node(k);
+					return current->right;
+				}
+			}
+		}
+	}
+
+	template <class T> Node<T>* Node<T>::add(char k, T value) {
+		Node<T>* current = this;
+		while(true) {
+			if(current->key == k) return current;
+			if(k < current->key) {
+				if(current->left) { 
+					current = current->left;
+				} else {
+					current->left = new Node(k, value);
+					return current->left;
+				}
+			} else {
+				if(current->right) { 
+					current = current->right;
+				} else {
+					current->right = new Node(k, value);
+					return current->right;
+				}
+			}
+		}
+	}
+
+	// Tree
+
+
+	template <class T> Tree<T>::Tree() {
+		this->root = nullptr;
+	}
+
+	template <class T> Tree<T>::~Tree() {
+		if(this->root != nullptr) this->root->deleteAll();
+	}
+
+	template <class T> void Tree<T>::set(string str, T value) {
+		Node<T>** current = &root;
+		Node<T>* last = nullptr;
+		for(int i = 0; i < str.length(); i++) {
+			if(*current == nullptr) {
+				*current = new Node<T>(str[i]);
+				last = *current;
+				current = &((*current)->forward);
+			} else {
+				Node<T>* tmp = (*current)->add(str[i]);
+				last = tmp;
+				current = &(tmp->forward);
+			}
+		}
+		last->value = value;
+		return;
+	}
+
+	template <class T> T Tree<T>::get(string str) {
+		Node<T>* current = root;
+		Node<T>* last;
+		
+		for(int i = 0; i < str.length(); i++) {
+			if(current == nullptr) return T();
+			current = current->find(str[i]);
+			if(current == nullptr) return T();
+			last = current;
+			current = current->forward;
+		}
+		if(last == nullptr) return T();
+		return last->value;
+	}
+
+	template <class T> void Tree<T>::clear(string str) {
+	}
 }
 
 #endif
--- src/stringtree_test.cc
+++ src/stringtree_test.cc
@@ -1,14 +1,67 @@
 #include <gtest/gtest.h>
+#include <string>
 
 #include "stringtree.h"
 
+using namespace std;
+using namespace StringTree;
+
 // Instantiate, check 
-TEST(StringTreeTest, NewDestroy) {
-  // freemap_t *map = freemap_new(127);
+TEST(StringTreeTest, TreeNewDestroy) {
+  Tree<uint> *t = new Tree<uint>();
+  delete t;
+}
 
-  // EXPECT_NE(map->bitmap, nullptr);
-  // EXPECT_EQ(map->total, 127);
-  // EXPECT_EQ(map->free, 127);
+TEST(StringTreeTest, NodeNewDestroy) {
+  Node<uint> *t = new Node<uint>('h');
 
-  // freemap_destroy(map);
+  t->deleteAll();
+}
+
+
+TEST(StringTreeTest, NodeAddFind) {
+  Node<uint> *t = new Node<uint>('m', 13);
+  t->add('g',7);
+  t->add('n',14);
+  t->add('x',24);
+  t->add('b',2);
+  Node<uint> *te = t->add('e',5);
+  t->add('t',20);
+
+  EXPECT_TRUE(t->find('a') == nullptr);
+  EXPECT_TRUE(t->find('c') == nullptr);
+  EXPECT_TRUE(t->find('z') == nullptr);
+  
+  EXPECT_TRUE(t->find('m') != nullptr);
+  EXPECT_TRUE(t->find('e') != nullptr);
+  EXPECT_TRUE(t->find('t') != nullptr);
+
+  EXPECT_EQ(t->find('m')->value, 13);
+  EXPECT_EQ(t->find('e')->value, 5);
+  EXPECT_EQ(t->find('t')->value, 20);
+
+  EXPECT_EQ(t->add('m',13), t);
+  EXPECT_EQ(t->add('e',5), te);
+
+  t->deleteAll();
+}
+
+TEST(StringTreeTest, SetGet) {
+  Tree<uint> *t = new Tree<uint>();
+
+  t->set("twenty five",25);
+
+  t->set("twenty four",24);
+  t->set("one",1);
+  t->set("fifteen",15);
+  t->set("six",6);
+  t->set("forty two",42);
+
+  EXPECT_EQ(t->get("forty two"), 42);
+
+  // t->clear("forty two");
+
+  // EXPECT_EQ(t->get("forty two"), 0);
+
+  delete t;
 }
